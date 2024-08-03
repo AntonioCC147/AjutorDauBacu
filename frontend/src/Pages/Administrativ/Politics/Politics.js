0710eebc034ea6543bb411963b300935d8a5cd06
AntonioCC147 <77618117+AntonioCC147@users.noreply.github.com>
2024-08-03 12:19:29 +0300

    Added Politica de Confidentialitate

--- frontend/src/Pages/Administrativ/Politics/Politics.js
+++ frontend/src/Pages/Administrativ/Politics/Politics.js
@@ -6,26 +6,133 @@ export default function Politics() {
             title={"Politica de Confidențialitate"}
             text={
                 <>
-                    <span class="indent">Lorem</span> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-                        standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
-                        make a type specimen book. It has survived not only five centuries, but also the leap into electronic
-                        typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
-                        sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker
-                        including versions of Lorem Ipsum.
+                    <b>Cuvânt înainte:</b>
                     <br/>
-                    <span class="indent">Lorem</span> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-                        standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
-                        make a type specimen book. It has survived not only five centuries, but also the leap into electronic
-                        typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
-                        sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker
-                        including versions of Lorem Ipsum.
+                    <span className="indent">Această</span> Politică de Confidențialitate descrie politica societății GEORGESCU
+                        RADU-ANDREI PERSOANĂ FIZICĂ AUTORIZATĂ, Pitești Bd Libertății nr. 11, Argeș, email: ajutordaubacu@gmail.com,
+                        cu privire la colectarea, utilizarea și partajarea datelor personale, date colectate dacă utilizați site-ul
+                        nostru (https://ajutordaubacu.ro), numit “Serviciul”. Prin accesarea și utilizarea Serviciului nostru, îți
+                        exprimi acordul cu privire la colectarea, utilizarea și partajarea informațiilor și datelor personale puse la
+                        dispoziție în conformitate cu această Politică de confidențialitate. Dacă nu sunteți de acord cu aceasta, vă
+                        rugăm să nu accesați sau să utilizați Serviciul nostru.
                     <br/>
-                    <span class="indent">Lorem</span> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-                        standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
-                        make a type specimen book. It has survived not only five centuries, but also the leap into electronic
-                        typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
-                        sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker
-                        including versions of Lorem Ipsum.
+                    <span className="indent">Putem</span> modifica această Politică de confidențialitate în orice moment, fără notificare
+                        prealabilă, și vom posta varianta revizuită și data intrării în vigoare. Politica revizuită va intra în vigoare
+                        în termen de 180 de zile de la data publicării și pentru utilizatorii care au acceptat vechea Politică.
+                        Accesul la Serviciu și utilizarea acestuia după acest termen implică acceptarea automată a Politicii
+                        revizuite. În consecință, recomandăm consultarea periodică a paginii de Politică de confidențialitate.
+                    <br/><br/>
+                    <b>Informațiile pe care le colectăm:</b>
+                    <br/>
+                    <span className="indent">Colectăm</span> și procesăm următoarele informații puse la dispoziție de utilizator
+                        în legătură cu accesarea și utilizarea Serviciului: Nume, Adresă, Email, Informații Plată.
+                    <br/><br/>
+                    <b>Cum colectăm aceste informații:</b>
+                    <br/>
+                    <span className="indent">Colectăm</span> și procesăm informațiile în situațiile următoare:
+                    <br/>
+                    <span className="indent">(1)</span> La deschiderea unui cont nou pe site: Nu colectăm informații personale
+                        în acest context.
+                    <br/>
+                    <span className="indent">(2)</span> Atunci când interacționezi cu site-ul: Utilizăm cookie-uri, inclusiv
+                        cookie-uri necesare pentru funcționarea site-ului și alte tipuri de cookie-uri conform Politicii Cookie.
+                        Acestea ne ajută să îmbunătățim experiența utilizatorului și să analizăm utilizarea site-ului.
+                    <br/>
+                    <span className="indent">(3)</span> Pe baza consimțământului (ex: abonare la Newsletter): Nu colectăm
+                        informații personale pentru abonarea la newsletter în prezent.
+                    <br/>
+                    <span className="indent">(4)</span> Când ne contactezi direct sau prin formularele de contact: Colectăm
+                        informațiile pe care le furnizezi, cum ar fi numele, adresa de email și orice alte detalii incluse în
+                        mesajul tău. Aceste informații sunt utilizate pentru a răspunde la solicitările tale și pentru a
+                        îmbunătăți serviciul nostru.
+                    <br/>
+                    <span className="indent">(5)</span> Când efectuezi o plată: Colectăm informațiile necesare pentru procesarea
+                        plății, dar nu stocăm datele cardurilor pe serverul nostru. Plățile sunt realizate securizat prin
+                        intermediul unui procesator de plăți (ex: PayPal, Stripe, MobilPay, PayU, etc.), care gestionează în
+                        siguranță datele tale de plată.
+                    <br/><br/>
+                    <b>Cum folosim informațiile colectate:</b>
+                    <br/>
+                    <span className="indent">Folosim</span> informațiile colectate în următoarele scopuri: Procesare plăți cu card, 
+                        Procesarea comenzilor, Informații administrative, Consimțământ Termeni & Condiții.
+                    <br/>
+                    <span className="indent">Dacă</span> vom intenționa să folosim informațiile colectate pentru oricare alt scop,
+                        îți vom cere înainte de utilizare consimțământul, și numai după primirea consimțământului, și numai pentru
+                        scopurile pentru care a fost cerut, vor fi folosite în consecință. Excepția poate fi numai în legătură cu
+                        cerințe sau obligații impuse de lege.
+                    <br/><br/>
+                    <b>Cu cine partajăm informațiile:</b>
+                    <br/>
+                    <span className="indent">Nu</span> vom partaja informațiile personale colectate către terțe părți, fără
+                        consimțământul tău, cu excepția unor situații limitate, cum ar fi cele de mai jos.
+                    <br/>
+                    <span className="indent">Condiționăm</span> transferul către aceste terțe părți de utilizarea informațiilor
+                        numai în scopul pentru care au fost colectate și transferate, și ne-stocarea acestora mai mult decât
+                        termenul agreat la colectare pentru îndeplinirea scopului.
+                    <br/>
+                    <span className="indent">Este</span> posibil să partajăm informațiile colectate pentru următoarele: (1)
+                        pentru respectarea unor obligații legale, sau aplicarea unor decizii judecătorești; (2) să demonstrăm
+                        colectarea acordului pentru această Politică de confidențialitate; (3) să răspundem unor reclamații în
+                        legătură cu încălcarea unor drepturi ale unor terțe părți, de către Serviciul nostru. În situația în care
+                        Serviciul nostru sau compania fuzionează sau este achiziționată de către o altă companie, informațiile ]
+                        colectate vor reprezenta active transferate către noul proprietar.
+                    <br/><br/>
+                    <b>Stocarea informațiilor colectate:</b>
+                    <br/>
+                    <span className="indent">Vom</span> păstra informațiile colectate pentru maxim 24 luni sau atât timp cât este
+                        necesar să fie îndeplinit scopul pentru care au fost colectate, așa cum a fost detaliat în prezenta Politică
+                        de confidențialitate. Vom fi obligați să păstrăm anumite informații pe termen mai lung, cum ar fi
+                        înregistrările contabile impuse de legislația în vigoare, sau pentru alte scopuri legitime, cum ar fi:
+                        aplicarea legii, prevenirea fraudelor.
+                    <br/>
+                    <span className="indent">Datele</span> anonime și informațiile agregate, care nu te pot identifica (direct
+                        sau indirect), ar putea fi stocate pe termen nedeterminat.
+                    <br/><br/>
+                    <b>Drepturile tale:</b>
+                    <br/>
+                    <span className="indent">Pentru</span> În funcție de legislația aplicabilă, ai dreptul de acces și rectificare
+                        sau ștergere a datelor tale personale colectate de către noi, ai dreptul să primești o copie a datelor
+                        colectate, ai dreptul de a te opune sau restricționa procesarea anumitor date personale, ai dreptul de a te
+                        adresa autorității competente cu petiții sau reclamații cu privire la drepturile tale. Pentru a-ți
+                        exercita aceste drepturi, ne poți contacta astfel: Georgescu Radu Andrei, Pitești, Bd. Libertății nr. 11,
+                        ajutordaubacu@gmail.com. Îți vom răspunde la cerere în conformitate cu legislația aplicabilă, în termen de
+                        30 zile.
+                    <br/>
+                    <span className="indent">Ai</span> posibilitatea să te opui prelucrării pentru comunicări de marketing sau
+                        analizei pentru publicitate personalizată, prin modificarea setărilor sau direct prin email la adresa
+                        ajutordaubacu@gmail.com.
+                    <br/>
+                    <span className="indent">Te</span> rugăm să iei în considerare că anumite informații colectate sunt necesare
+                        pentru funcționarea Serviciului, iar retragerea consimțământului poate face ca acesta să devină
+                        indisponibil pentru utilizare.
+                    <br/><br/>
+                    <b>Cookie-uri, urmărire:</b>
+                    <br/>
+                    <span className="indent">Pentru</span> mai multe detalii cu privire la cookie-urile și tehnologiile utilizate,
+                        te rugăm să consulți Politica Cookie.
+                    <br/><br/>
+                    <b>Securitate:</b>
+                    <br/>
+                    <span className="indent">Securitatea</span> datelor și informațiilor colectate este importantă pentru noi, și
+                        vom implementa măsuri de securitate adecvate pentru a preveni pierderea, alterarea și accesul neautorizat
+                        la datele colectate. Totuși, date fiind riscurile inerente, nu putem garanta securitate absolută și, în
+                        consecință, nu putem garanta securitatea informațiilor colectate.
+                    <br/><br/>
+                    <b>Link-uri externe:</b>
+                    <br/>
+                    <span className="indent">Serviciul</span> nostru poate afișa link-uri către alte site-uri care nu sunt
+                        operate de noi. Această Politică de confidențialitate nu are legătură cu politicile și practicile altor
+                        site-uri, inclusiv dar fără a se limita la site-uri accesate prin intermediul unor link-uri publicate de
+                        acest Serviciu. Îți recomandăm să consulți Politicile de confidențialitate ale oricăror alte site-uri pe
+                        care le vizitezi, pornind de la acest Serviciu. Nu avem control și nu ne asumăm responsabilitatea pentru
+                        conținutul, politicile de confidențialitate și practicile altor site-uri.
+                    <br/><br/>
+                    <b>Contact / DPO:</b>
+                    <br/>
+                    <span className="indent">Dacă</span> ai întrebări sau nelămuriri cu privire la procesarea informațiilor /
+                        datelor personale colectate de către noi, te rugăm să ne contactezi, „În atenția Responsabilului cu
+                        datele personale”, la adresa: Georgescu Radu Andrei, Pitești, Bd. Libertății nr. 11, email: ajutordaubacu@gmail.com.
+                        Vom răspunde solicitării în concordanță cu prevederile legale aplicabile în vigoare.
                 </>
             }
         />
